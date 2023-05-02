9681f2c78fdf622725f007e7df453aba53cd6e4a
Derek den Haas <d.haas@directcode.com>
2023-05-02 13:57:08 +0200

    Login Registratie frontend gemaakt, User klasse gemaakt.

---
+++ BestelSysteem/Pages/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BestelSysteem
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
