00ab083d6dd55d707e8491f222ae49be20516667
Derek den Haas <d.haas@directcode.com>
2023-05-09 09:40:42 +0200

    Show product and stock

--- BestelSysteem/Pages/Index.cshtml.cs
+++ BestelSysteem/Pages/Index.cshtml.cs
@@ -1,19 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BestelSysteem.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Raven.Client.Documents;
 
 namespace BestelSysteem.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IDocumentStore _documentStore;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IDocumentStore documentStore)
     {
         _logger = logger;
+        _documentStore = documentStore;
     }
 
     public void OnGet()
     {
 
+    }
+
+    public IActionResult OnPostLogin(string username, string password)
+    {
+        using (var session = _documentStore.OpenSession())
+        {
+            var user = session.Query<User>()
+                .FirstOrDefault(u => u.FirstName == username && u.Password == password);
+
+            if (user != null)
+            {
+                // gebruiker is gevonden en gevalideerd, stuur door naar Overview-pagina
+                return RedirectToPage("/Overview");
+            }
+            else
+            {
+                // Pass the error message to the frontend
+                ViewData["ErrorMessage"] = "Incorrect username or password.";
+
+                return Page();
+            }
+        }
     }
 }
--- BestelSysteem/Pages/Overview.cshtml.cs
+++ BestelSysteem/Pages/Overview.cshtml.cs
@@ -1,12 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using BestelSysteem.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
 
 namespace MyApp.Namespace
 {
     public class OverviewModel : PageModel
     {
+        private readonly IDocumentStore _documentStore;
+
+        public OverviewModel(IDocumentStore documentStore)
+        {
+            _documentStore = documentStore;
+        }
+
+        public List<Product> Products { get; set; }
+
         public void OnGet()
         {
+            using (var session = _documentStore.OpenSession())
+            {
+                var products = session.Query<Product>().ToList();
+                foreach (var product in products)
+                {
+                    var counter = session.CountersFor(product.Id).Get("Stock");
+                    product.Stock = (int)(counter ?? 0); // if the counter doesn't exist, default to 0
+                }
+                Products = products;
+            }
         }
+
+
+
     }
 }
